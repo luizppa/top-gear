9b044c1054d8b9a769a43fe90b0568c9dde5daa4
Luiz Philippe <luiz@phari.solutions>
2018-10-26 13:44:48 -0300

    Fixed some sound issues

--- helpers/sounds.c
+++ helpers/sounds.c
@@ -1,19 +1,17 @@
 #include "sounds.h"
 
+// Play music from the beginning
 void start_music(ALLEGRO_AUDIO_STREAM* music){
+  al_attach_audio_stream_to_mixer(music, al_get_default_mixer());
   al_set_audio_stream_playing(music, true);
 }
 
+// Stop the music execution
 void stop_music(ALLEGRO_AUDIO_STREAM* music){
   al_set_audio_stream_playing(music, false);
 }
 
+// Set the music to be played
 ALLEGRO_AUDIO_STREAM* set_music(char* file){
   return al_load_audio_stream(file, 4, SAMPLES_PER_BUFFER);
-}
-
-void change_music(ALLEGRO_AUDIO_STREAM* music, char* file){
-  al_set_audio_stream_playing(music, false);
-  music = al_load_audio_stream("assets/music/01_title.ogg", 4, SAMPLES_PER_BUFFER);
-  al_set_audio_stream_playing(music, true);
 }
